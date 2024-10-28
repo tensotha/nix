343c59bd9057aa768a3a2612657efcd4eac63e18
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-28 21:35:55 +0100

    add lualine, bufferline, telescope, treesitter, neotree and  fix theme setting

---
+++ config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,28 @@
+-- Themes
+return {
+
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
+	"typicode/bg.nvim",
+
+	"ellisonleao/gruvbox.nvim",
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+
+	{
+		"rose-pine/nvim",
+		name = "rose-pine",
+	},
+
+	"sainnhe/everforest",
+
+	"savq/melange-nvim"
+}
