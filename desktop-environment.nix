efe1aeb927a7f590bc6c4fb432f36761aac009f6
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-28 22:24:42 +0100

    remove awesome wm

--- desktop-environment.nix
+++ desktop-environment.nix
@@ -5,12 +5,6 @@ programs.hyprland.enable = true; # enable Hyprland
 environment.sessionVariables.NIXOS_OZONE_WL = "1";
 services.xserver = {
   enable = true;
-  windowManager.awesome = {
-    enable = true;
-    luaModules = with pkgs.luaPackages; [
-        luarocks # is the package manager for Lua modules
-        luadbi-mysql # Database abstraction layer
-    ];
   };
   displayManager.gdm.enable = true;
   desktopManager.gnome.enable = true;
