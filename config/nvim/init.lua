4bd7b90154861513f5d8bf2838ab8360d56f4374
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-28 21:28:42 +0100

    use a modular structure with nvim and lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,53 +1,8 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
+-- Handle plugins with lazy.nvim
+require("core.lazy")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- General Neovim keymaps
+require("core.keymaps")
 
--- plugins here means the file inside of the "lua folder"
-require("lazy").setup("plugins")
-
--- Telescope
-local builtin = require("telescope.builtin")
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left <CR>', {})
---Treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = {
-        "lua",
-        "javascript",
-        "python",
-        "go"
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-})
-
--- Neotree
-
-
--- Theme
-
-require("tokyonight").setup()
-
-vim.cmd.colorscheme = "tokyonight"
+-- Other options
+require("core.options")
