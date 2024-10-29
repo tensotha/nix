127cac94b5e259acdeebc20824fd7b1c3e50e73c
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-29 22:34:11 +0100

    remove bufferline

--- config/nvim/lua/plugins/bufferline.lua
+++
@@ -1,5 +0,0 @@
--- See current buffers at the top of the editor
-return {
-		{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
-     require("bufferline").setup{}
-}
