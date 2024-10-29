17b314fe0c57caf2c42fa9388378ec5c4713df50
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-29 20:20:52 +0100

    auto template hyprland and waybar configs to the xdg config path

--- desktop-environment.nix
+++ desktop-environment.nix
@@ -4,7 +4,20 @@
 xdg.portal.enable = true;
 xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 programs.hyprland.enable = true; # enable Hyprland
+xdg.configFile.hyprland = {
+    source = ./config/hypr;
+    recursive = true;
+};
+
+
+
 programs.waybar.enable = true;
+xdg.configFile.waybar = {
+    source = ./config/waybar;
+    recursive = true;
+};
+
+
 environment.sessionVariables.NIXOS_OZONE_WL = "1";
 services.xserver = {
     enable = true;
