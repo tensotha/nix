f50bd04be7279e09a565d6c11c6560f7c8e82c59
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-30 23:11:09 +0100

    merge flakes to 1

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "macos/linux flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, nixvim,  darwin, ... }: {
+          systems = [ "x86_64-linux" "aarch64-darwin"];
+       darwinConfigurations = {
+      tensotha = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./hosts/macos/configuration.nix
+          ./hosts/macos/home.nix
+          inputs.nixvim.nixDarwinModules.nixvim
+          home-manager.darwinModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+        nixosConfigurations.tensotha-nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
+      modules = [
+       ./hosts/linux/configuration.nix 
+
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.tensotha = import ./hosts/linux/home.nix;
+       }
+      ];
+    };
+  };
+}
