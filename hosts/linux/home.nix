4df2c11c7e51caafabf633ec76a1fdfbed30d42e
tensotha <abdulhakim.alsharif@hotmail.com>
2024-10-31 00:04:51 +0100

    add starship

--- hosts/linux/home.nix
+++ hosts/linux/home.nix
@@ -1,27 +1,106 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 {
   imports = [
     # NOTE: The first thing you will want to do is uncommented on of the three imports below
     # depending on which module you chose to use to install Nixvim.
     #
     # Uncomment if you are using the home-manager module
-    inputs.nixvim.homeManagerModules.nixvim
+    #inputs.nixvim.homeManagerModules.nixvim
     # Uncomment if you are using the nixos module
-    # inputs.nixvim.nixosModules.nixvim
+    #inputs.nixvim.nixosModules.nixvim
     # Uncomment if you are using the nix-darwin module
     #inputs.nixvim.nixDarwinModules.nixvim
 
-    ../../home-manager/common/nvim/nvim.nix
+    # ../../home-manager/common/nvim/nvim.nix
+    ../../home-manager/common/starship/starship.nix
 
     ];
 
   users.users.tensotha = {
   name = "tensotha";
-  home = "/home/tensotha";
+  home = "/Users/tensotha";
 };
 
 home-manager.users.tensotha = { pkgs, ... }: {
-  home.packages = [];
+ home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+    vscodium
+    hyprcursor
+    neofetch
+    firefox
+    kitty
+
+    vesktop
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+xdg.configFile.waybar = {
+    source = ./config/waybar;
+    recursive = true;
+};
+
+xdg.configFile.hypr = {
+    source = ./config/hypr;
+    recursive = true;
+};
   programs.bash.enable = true;
 
   # The state version is required and should stay at the version you
