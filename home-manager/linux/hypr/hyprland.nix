160cbc9f84a38399eb12613fccf847aeb65edb67
abdulhakim alsharif <abdulhakim.alsharif@hotmail.com>
2024-10-31 22:39:19 +0100

    move hyprland.conf

--- home-manager/linux/hypr/hyprland.nix
+++ home-manager/linux/hypr/hyprland.nix
@@ -1,14 +1,15 @@
 { pkgs, ...}: {
-programs.hyprland.enable = true; # enable Hyprland
-
+  wayland.windowManager.hyprland.enable = true; # enable Hyprland
 programs.waybar.enable = true;
 programs.hyprlock.enable = true;
 xdg.configFile.waybar = {
-    source = ./waybar;
+# environment.sessionVariables.NIXOS_OZONE_WL = "1";
+   source = ./waybar;
     recursive = true;
 };
 
 xdg.configFile.hypr = {
-    source = ./hyprland.conf;
+    source = ./hypr;
+    recursive = true;
 };
 }
--- hosts/linux/desktop-environment.nix
+++ hosts/linux/desktop-environment.nix
@@ -3,11 +3,7 @@
 {
 xdg.portal.enable = true;
 xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
-programs.hyprland.enable = true; # enable Hyprland
 
-programs.waybar.enable = true;
-programs.hyprlock.enable = true;
-environment.sessionVariables.NIXOS_OZONE_WL = "1";
 services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
