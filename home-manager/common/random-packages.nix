f7cefbf2d2080e37b1ad9bb761e71dd00f0edf9e
abdulhakim alsharif <abdulhakim.alsharif@hotmail.com>
2024-11-02 13:57:03 +0100

    install copyq

--- home-manager/common/random-packages.nix
+++ home-manager/common/random-packages.nix
@@ -38,7 +38,7 @@ programs.yazi = {
     xz
     unzip
     p7zip
-
+    copyq
     # utils
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
